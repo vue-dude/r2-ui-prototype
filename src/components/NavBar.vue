53adef93785a21a767b9299f11e771431e5e54b6
leonhard <leonhard@mpdl.mpg.de>
2021-01-18 12:38:50 +0100

    feat RD-85: Add 'Edmond' - logo / brand on navbar.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,11 @@
             <div class="border left"></div>
             <div class="border right"></div>
         </div>
-        <div class="left-adjusted-text headline" v-html="headline"></div>
+        <!-- <div class="left-adjusted-text headline" v-html="headline"></div> -->
+        <div class="left-adjusted-text brand">
+            <div class="edmond-logo"></div>
+            <div class="fade-bg"></div>
+        </div>
     </div>
 </template>
 
