e5edc83b594d83de5979b9b7cf48eed99a83c39a
leonhard <leonhard@mpdl.mpg.de>
2021-02-08 10:01:16 +0100

    feat refac RD-85: Prepare for external integration / set main route to 'ui', cleanup unused stuff

--- src/js/Datasource.js
+++ src/js/Datasource.js
@@ -35,24 +35,15 @@ function Datasource() {
 
     const config = {
         pathMap: base.pathMap,
-        saveApi: `${base.cms}update`,
-        uploadApi: `${base.cms}upload`,
-        mediaApi: `${base.cms}media`,
-        pulishApi: `${base.cms}publish`,
-        authApi: `${base.cms}authenticate`,
-        authConfirmApi: `${base.cms}auth-confirm`,
-        ckEditor: 'lib/ckeditor/ckeditor.js',
         structure: null,
-        home: ['structure', 'startpage'],
-        sessionTimeoutMsec: 10000,
         translations: {},
-        setup: {},
-        circleNav: []
+        setup: {}
     }
 
     console.log('DS:INIT config = ', config)
 
     const getConfig = () => config
+    this.getConfig = getConfig
 
     const getPath = path => {
         if (path.substr(0, 1) === '/') {
@@ -69,36 +60,34 @@ function Datasource() {
     }
     this.getPath = getPath
 
-    const updateMediaPaths = html => {
-        if (_.isString(html)) {
-            let path = getPath('/media/') // flat file mode
-            if (base.cms && base.cms !== '/') {
-                path = `${base.cms}media/` // api / cms mode
-            }
-            html = html.split('src="/media/').join(`src="${path}`)
-        }
-        if (!globals.WEBP_ENABLED) {
-            html = html.split('.webp"').join('.jpg"')
-        }
-        return html
-    }
-    this.updateMediaPaths = updateMediaPaths
+    // const updateMediaPaths = html => {
+    //     if (_.isString(html)) {
+    //         let path = getPath('/media/') // flat file mode
+    //         if (base.cms && base.cms !== '/') {
+    //             path = `${base.cms}media/` // api / cms mode
+    //         }
+    //         html = html.split('src="/media/').join(`src="${path}`)
+    //     }
+    //     if (!globals.WEBP_ENABLED) {
+    //         html = html.split('.webp"').join('.jpg"')
+    //     }
+    //     return html
+    // }
+    // this.updateMediaPaths = updateMediaPaths
 
     const get = async (api, data = {}, options = {}) => {
         return axios.create().get(getPath(api), data, options)
     }
 
-    const post = async (api, data = {}, options = {}) => {
-        // data.token = globals.getAdminToken()
-        console.log('DS:post api = ', api)
-        return axios.create().post(getPath(api), data, options)
-    }
+    // const post = async (api, data = {}, options = {}) => {
+    //     return axios.create().post(getPath(api), data, options)
+    // }
 
-    const getValidI18nKey = i18nKey => {
-        const trns = _.get(config.translations, `${globals.getLocale()}.${i18nKey}.__0`)
-        return _.isString(trns) ? `${i18nKey}.__0` : i18nKey
-    }
-    this.getValidI18nKey = getValidI18nKey
+    // const getValidI18nKey = i18nKey => {
+    //     const trns = _.get(config.translations, `${globals.getLocale()}.${i18nKey}.__0`)
+    //     return _.isString(trns) ? `${i18nKey}.__0` : i18nKey
+    // }
+    // this.getValidI18nKey = getValidI18nKey
 
     // only UI elements
     const setTranslationFallbacks = (translations, lng, key, i18n) => {
@@ -150,6 +139,8 @@ function Datasource() {
             })
         })
     }
+    this.getInitialData = () => getInitialData()
+
 
     const getStructure = () => {
         if (config.structure) {
@@ -175,97 +166,102 @@ function Datasource() {
             })
             .catch(error => console.warn('DS:getTranslations ERROR error.message = ', error.message))
     }
+    this.getTranslations = getTranslations
+
 
     const generateStructure = raw => {
         const navigation = {}
         const routes = []
+        //
+        // Commented out code as example for creating router and navigation by structure
+        //
         // navigation
-        _.each(raw.structure, (itemL1, keyL1) => {
-            const ui = _.get(raw, `ui.nav.${keyL1}`) || {}
-            const nav = (navigation[keyL1] = { ...itemL1, ...ui })
-            nav.i18nKey = `nav.${keyL1}.label`
-            nav.sub = {}
-            // router
-            const mainRoute = {
-                path: keyL1,
-                // name: key,
-                // alias: `${key}/*`,
-                props: true,
-                params: {
-                    mainKey: keyL1
-                },
-                meta: {
-                    mainKey: keyL1,
-                    subKey: null,
-                    i18nKey: `nav.${keyL1}.label`
-                },
-                children: []
-            }
-            routes.push(mainRoute)
+        // _.each(raw.structure, (itemL1, keyL1) => {
+        //     const ui = _.get(raw, `ui.nav.${keyL1}`) || {}
+        //     const nav = (navigation[keyL1] = { ...itemL1, ...ui })
+        //     nav.i18nKey = `nav.${keyL1}.label`
+        //     nav.sub = {}
+        //     // router
+        //     const mainRoute = {
+        //         path: keyL1,
+        //         // name: key,
+        //         // alias: `${key}/*`,
+        //         props: true,
+        //         params: {
+        //             mainKey: keyL1
+        //         },
+        //         meta: {
+        //             mainKey: keyL1,
+        //             subKey: null,
+        //             i18nKey: `nav.${keyL1}.label`
+        //         },
+        //         children: []
+        //     }
+        //     routes.push(mainRoute)
 
-            _.each(itemL1.sub, (itemL2, keyL2) => {
-                // navigation
-                itemL2 = _.isPlainObject(itemL2) ? itemL2 : {}
-                itemL2.i18nKey = `nav.${keyL1}.sub.${keyL2}.label`
-                itemL2.mainNav = itemL2.mainNav !== false
-                itemL2.circleNav = itemL2.circleNav !== false
-                // add to main navigation L2
-                if (itemL2.mainNav) {
-                    navigation[keyL1].sub[keyL2] = itemL2
-                }
-                // add to circle navigation L2
-                if (itemL2.circleNav) {
-                    config.circleNav.push(`/${keyL1}/${keyL2}`)
-                }
-                // router
-                mainRoute.children.push({
-                    path: keyL2,
-                    props: true,
-                    params: {
-                        subKey: keyL2
-                    },
-                    meta: {
-                        mainKey: keyL1,
-                        subKey: keyL2,
-                        i18nKey: `nav.${keyL1}.sub.${keyL2}.label`
-                    }
-                })
-                const subRoute = _.last(mainRoute.children)
-                subRoute.children = []
+        //     _.each(itemL1.sub, (itemL2, keyL2) => {
+        //         // navigation
+        //         itemL2 = _.isPlainObject(itemL2) ? itemL2 : {}
+        //         itemL2.i18nKey = `nav.${keyL1}.sub.${keyL2}.label`
+        //         itemL2.mainNav = itemL2.mainNav !== false
+        //         itemL2.circleNav = itemL2.circleNav !== false
+        //         // add to main navigation L2
+        //         if (itemL2.mainNav) {
+        //             navigation[keyL1].sub[keyL2] = itemL2
+        //         }
+        //         // add to circle navigation L2
+        //         if (itemL2.circleNav) {
+        //             config.circleNav.push(`/${keyL1}/${keyL2}`)
+        //         }
+        //         // router
+        //         mainRoute.children.push({
+        //             path: keyL2,
+        //             props: true,
+        //             params: {
+        //                 subKey: keyL2
+        //             },
+        //             meta: {
+        //                 mainKey: keyL1,
+        //                 subKey: keyL2,
+        //                 i18nKey: `nav.${keyL1}.sub.${keyL2}.label`
+        //             }
+        //         })
+        //         const subRoute = _.last(mainRoute.children)
+        //         subRoute.children = []
 
-                _.each(itemL2.sub, (itemL3, keyL3) => {
-                    if (keyL3 !== '__0') {
-                        // navigation
-                        itemL3 = _.isPlainObject(itemL3) ? itemL3 : {}
-                        itemL3.i18nKey = `nav.${keyL1}.sub.${keyL2}.sub.${keyL3}.label`
-                        itemL3.mainNav = itemL3.mainNav !== false
-                        itemL3.circleNav = itemL3.circleNav !== false
-                        // add to main navigation L3
-                        if (itemL3.mainNav) {
-                            navigation[keyL1].sub[keyL2].sub[keyL3] = itemL3
-                        }
-                        // add to circle navigation L3
-                        if (itemL3.circleNav) {
-                            config.circleNav.push(`/${keyL1}/${keyL2}/${keyL3}`)
-                        }
-                        // router
-                        subRoute.children.push({
-                            path: keyL3,
-                            props: true,
-                            params: {
-                                keyL3
-                            },
-                            meta: {
-                                mainKey: keyL1,
-                                subKey: keyL2,
-                                keyL3,
-                                i18nKey: `nav.${keyL1}.sub.${keyL2}.sub.${keyL3}.label`
-                            }
-                        })
-                    }
-                })
-            })
-        })
+        //         _.each(itemL2.sub, (itemL3, keyL3) => {
+        //             if (keyL3 !== '__0') {
+        //                 // navigation
+        //                 itemL3 = _.isPlainObject(itemL3) ? itemL3 : {}
+        //                 itemL3.i18nKey = `nav.${keyL1}.sub.${keyL2}.sub.${keyL3}.label`
+        //                 itemL3.mainNav = itemL3.mainNav !== false
+        //                 itemL3.circleNav = itemL3.circleNav !== false
+        //                 // add to main navigation L3
+        //                 if (itemL3.mainNav) {
+        //                     navigation[keyL1].sub[keyL2].sub[keyL3] = itemL3
+        //                 }
+        //                 // add to circle navigation L3
+        //                 if (itemL3.circleNav) {
+        //                     config.circleNav.push(`/${keyL1}/${keyL2}/${keyL3}`)
+        //                 }
+        //                 // router
+        //                 subRoute.children.push({
+        //                     path: keyL3,
+        //                     props: true,
+        //                     params: {
+        //                         keyL3
+        //                     },
+        //                     meta: {
+        //                         mainKey: keyL1,
+        //                         subKey: keyL2,
+        //                         keyL3,
+        //                         i18nKey: `nav.${keyL1}.sub.${keyL2}.sub.${keyL3}.label`
+        //                     }
+        //                 })
+        //             }
+        //         })
+        //     })
+        // })
 
         return {
             navigation,
@@ -277,196 +273,6 @@ function Datasource() {
         }
     }
 
-    const sanitizeFilename = input => {
-        const allowedEndings = {
-            jpeg: true,
-            jpg: true,
-            png: true,
-            gif: true,
-            webp: true
-        }
-        const regx = new RegExp('^[a-zA-Z0-9()_.-]+$', 'g')
-        const fn = input.split('.')
-        const postfix = fn.pop().toLowerCase()
-        let prefix = fn.length > 0 ? fn.join('-') : ''
-        const isUsable = !allowedEndings[postfix.toLowerCase()] || prefix === '' ? false : true
-        const isMd5ed = !regx.test(prefix)
-        prefix = isMd5ed ? CryptoJS.MD5(prefix).toString() : prefix
-        return {
-            raw: input,
-            prefix,
-            postfix,
-            processedName: prefix === '' ? postfix : `${prefix}.${postfix}`,
-            isUsable,
-            isMd5ed
-        }
-    }
-
-    // cms / ck-editor 4
-    const ckUploadFileRequest = ckEvt => {
-        const upload = {}
-        upload.base64 = ckEvt.data.fileLoader.data
-        const cleaned = sanitizeFilename(ckEvt.data.fileLoader.file.name)
-        if (!cleaned.isUsable) {
-            window.alert(`Filetype, filename or ending "${cleaned.postfix}" not valid`)
-            return Promise.resolve(resolved => ({}))
-        }
-        upload.fileName = cleaned.processedName
-        upload.key = `${globals.getLocale()}.${globals.getFocusedKey()}`
-        return post(config.uploadApi, upload)
-            .then(res => {
-                if (res.data.success === true) {
-                    res.data.path = `${config.mediaApi}/${upload.key.split('.').join('/')}/${upload.fileName}`
-                    res.data.previewPath = `${config.mediaApi}/${res.data.previewKey}/${upload.key
-                        .split('.')
-                        .join('/')}/${upload.fileName}`
-                    res.data.key = upload.key
-                }
-                res.data.error = null
-                return res.data
-            })
-            .catch(error => {
-                console.warn('DS:getTranslations ERROR error.message = ', error.message)
-                return {
-                    success: false,
-                    error: error.message
-                }
-            })
-    }
-    this.ckUploadFileRequest = ckUploadFileRequest
-
-    const ckSaveChanges = async (silent = false) => {
-        const saveData = {
-            data: []
-        }
-        _.each(changedTranslations, (value, key) => {
-            // extremely important! only send cleaned media paths to server!
-            value = value.split(config.mediaApi).join('/media')
-            saveData.data.push({
-                key: `${key}`,
-                value
-            })
-        })
-        return post(config.saveApi, saveData)
-            .then(res => {
-                hardUpdateTranslations(globals.getLocale(), res.data)
-                silent ? null : window.alert('Changes successfully saved')
-                return { success: true }
-            })
-            .catch(evt => {
-                window.alert(`Changes save error:\n${evt}`)
-                return evt
-            })
-    }
-    this.ckSaveChanges = ckSaveChanges
-
-    // cms
-
-    const cmsPublish = async () => {
-        const res = await ckSaveChanges(true)
-        if (res.success === true) {
-            return post(config.pulishApi)
-                .then(res => {
-                    console.log('DS:cmsPublish res = ', res)
-                    window.alert('Changes successfully published')
-                    // globals.setCmsPublishFeedback({ success: true })
-                })
-                .catch(evt => {
-                    console.warn('DS:cmsPublish ERROR = ', evt)
-                    window.alert(`Changes publish error:\n${evt}`)
-                    // globals.setCmsPublishFeedback({ success: false })
-                    return evt
-                })
-        }
-    }
-
-    const cmsUpdateTranslations = async data => {
-        // needed also with ck!
-        const saveData = {
-            data: []
-        }
-        const loc = globals.getLocale()
-        _.each(data, (value, key) => {
-            value = value.replace(/(?:\r\n|\r|\n)/g, '') // <br>
-            value = value.replace(/>\s+</g, '><')
-            if (base.cms && base.cms !== '/') {
-                value = value.split(base.cms).join('/')
-            }
-            saveData.data.push({
-                key: `${loc}.${key}`,
-                value
-            })
-        })
-        return post(config.saveApi, saveData)
-            .then(res => {
-                hardUpdateTranslations(globals.getLocale(), res.data)
-            })
-            .catch(evt => {
-                console.warn('DS:cmsUpdateTranslations ERROR = ', evt)
-                return evt
-            })
-    }
-
-    const authenticate = async email => {
-        return post(config.authApi, { email }).then(res => {
-            if (globals.DEV_MODE) {
-                if (res.data.success) {
-                    // In dev mode, there is no need always to klick the mail confirmation
-                    globals.setAdminToken(res.data.token)
-                    cmsUpdateTranslations()
-                } else {
-                    window.alert('No valid access data')
-                }
-            }
-        })
-    }
-    this.authenticate = authenticate
-
-    const authenticationConfirm = async token => {
-        return post(config.authConfirmApi).then(res => {
-            if (res.data.success === true) {
-                globals.setAdminToken(token)
-            } else {
-                globals.setAdminToken(null)
-            }
-        })
-    }
-    this.authenticationConfirm = authenticationConfirm
-
-    const hardUpdateTranslations = (loc, trns) => {
-        // Its needed to kill the complete language set here before !!
-        config.translations[loc] = {}
-        globals.getI18n().vm.messages[loc] = {}
-        // write updates to locale:
-        addTranslations(trns)
-        globals.getI18n().setLocaleMessage(loc, config.translations[loc])
-        globals.getI18n().locale = loc
-    }
-
-    const changedTranslations = {}
-
-    const cmsUpdateTranslation = (trns, silent) => {
-        const key = globals.getFocusedKey()
-        const loc = globals.getLocale()
-        trns = _.isString(trns) ? trns : changedTranslations[`${loc}.${key}`] || null
-        silent = silent === true
-        if (_.isString(key) && _.isString(trns)) {
-            // different set here, key-as-string on changedTranslations vs.
-            // _.set key as datastructure in config.translations !
-            changedTranslations[`${loc}.${key}`] = trns
-            if (!silent) {
-                _.set(config.translations, `${loc}.${key}`, trns)
-            }
-        }
-    }
-    this.cmsUpdateTranslation = cmsUpdateTranslation
-
-    // publix
-
-    this.cmsPublish = cmsPublish
-    this.getConfig = getConfig
-    this.getTranslations = getTranslations
-    this.getInitialData = () => getInitialData()
 }
 
 export default Datasource
--- src/js/router.js
+++ src/js/router.js
@@ -1,51 +1,18 @@
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import DefaultPage from '../views/DefaultPage.vue'
-import About from '../views/About.vue'
-// import View1 from '../views/View1.vue'
 
 const routes = [
     {
         path: '/:pathMatch(.*)*',
-        beforeEnter: (to, from, next) => next('/home')
+        beforeEnter: (to, from, next) => next('/ui')
     },
     {
-        path: '/home',
-        name: 'home',
+        path: '/ui',
+        name: 'ui',
         props: true,
         component: DefaultPage
-    },
-    {
-        path: '/about',
-        name: 'about',
-        props: true,
-        // props: {
-        //     user: 'lacca!'
-        // },
-        component: About
-        // or lazy load via code splitting
-        // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    },
-    {
-        path: '/booo',
-        name: 'booo',
-        props: true,
-        redirect: 'home'
-    },
-    {
-        path: '/template-1',
-        name: 'template-1',
-        component: () => import('../views/Template-1.vue')
     }
-    // {
-    //     path: '/view-1',
-    //     name: 'view-1',
-    //     component: View1
-    // }
 ]
-
-const createRoutes = value => {
-    console.log('obj:arrow value = ', value)
-}
 
 const router = createRouter({
     history: createWebHashHistory(), // createWebHistory createWebHashHistory
@@ -53,62 +20,8 @@ const router = createRouter({
 })
 
 const getRouter = config => {
+    // do the dynamic config by structure here
     return router
 }
 
 export default getRouter
-
-/** 
-import VueRouter from 'vue-router'
-import DefaultPage from '../views/DefaultPage.vue'
-
-
-const root = {
-    mode: 'hash', // hash history
-    scrollBehavior: () => ({ y: 0 }),
-    routes: null
-}
-
-const createDynamicConfig = config => {
-    root.routes = config.root
-    const rt = _.find(root.routes, { children: 'structure' })
-    rt.component = DefaultPage
-    rt.children = config.routes
-    root.routes.push(
-        {
-            path: '/admin',
-            beforeEnter: (to, from, next) => {
-                globals.touchedAdmin()
-                // setTimeout(() => next('/'), 100)
-                setTimeout(() => next(from), 100)
-            }
-        },
-        {
-            path: '/auth-confirm/:token',
-            beforeEnter: (to, from, next) => {
-                const token = to.path.split('/').pop()
-                datasource.authenticationConfirm(token)
-                setTimeout(() => next('/'), 100)
-            }
-        },
-        {
-            path: '/area51',
-            beforeEnter: (to, from, next) => {
-                globals.showDeviceInfoDelayed()
-                setTimeout(() => next(from), 100)
-            }
-        }
-    )
-    console.log('RT:createDynamicConfig routes root = ', root)
-    return root
-}
-// mode: ‘history’
-const getRouter = config => {
-    // Vue.use(VueRouter)
-    const router = new VueRouter(createDynamicConfig(config))
-    globals.registerRouter(router)
-    return router
-}
-export default getRouter
-
-**/
