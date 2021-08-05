b2d35537c890bc0681f2394be07b64dab2292c1f
hleonhard <leonhard@vimp.com>
2021-08-05 10:10:28 +0200

    feat refac: Replace the vue template box setup by a JSX render function

--- src/ui/components/Cage.vue
+++ src/ui/components/Cage.vue
@@ -1,27 +1,3 @@
-<template>
-    <div class="cage boxes" :class="[{ hidden: !showCage }, modalClass]">
-        <box class="animate" :config="boxes['main-nav']" :isModalOverlay="isModalOverlay"></box>
-        <box class="animate" :config="boxes['head-controls']" :isModalOverlay="isModalOverlay"></box>
-        <!--  -->
-        <box class="animate" :config="boxes['landing-page']" :isModalOverlay="isModalOverlay"></box>
-        <box class="animate" :config="boxes['search-page']" :isModalOverlay="isModalOverlay"></box>
-        <box class="animate" :config="boxes['search-page-facets-dn-inner']" :isModalOverlay="isModalOverlay"></box>
-        <box class="animate" :config="boxes['mywork-page']" :isModalOverlay="isModalOverlay"></box>
-        <!--  -->
-        <box class="animate" :config="boxes['dataset-view-public']" :isModalOverlay="isModalOverlay"></box>
-        <box class="animate" :config="boxes['dataset-view-private-content']" :isModalOverlay="isModalOverlay"></box>
-        <box class="animate" :config="boxes['dataset-view-private-infos']" :isModalOverlay="isModalOverlay"></box>
-        <!--  -->
-        <box class="animate" :config="boxes['dataset-controls']" :isModalOverlay="isModalOverlay"></box>
-        <box class="animate" :config="boxes['dataset-actions']" :isModalOverlay="isModalOverlay"></box>
-        <box class="animate" :config="boxes['r2-messages']" :isModalOverlay="isModalOverlay"></box>
-        <!--  -->
-        <box class="animate" :config="boxes['file-list']" :isModalOverlay="isModalOverlay"></box>
-
-        <box class="animate" :config="boxes['mock-config']" :isModalOverlay="isModalOverlay"></box>
-    </div>
-</template>
-
 <script>
 //
 import Box from '@/ui/components/Box.vue'
@@ -61,9 +37,65 @@ export default {
             }
             this.logicHandler.init()
         }, 400)
+    },
+    render() {
+        // This JSX render function replaces the deactivated template below
+        const makeBox = id => {
+            const props = {
+                config: this.boxes[id],
+                class: 'animate',
+                isModalOverlay: this.isModalOverlay
+            }
+            return <box {...props}></box>
+        }
+        const usedBoxesInOrder = [
+            'main-nav',
+            'head-controls',
+            'landing-page',
+            'search-page',
+            'search-page-facets-dn-inner',
+            'mywork-page',
+            'dataset-view-public',
+            'dataset-view-private-content',
+            'dataset-view-private-infos',
+            'dataset-controls',
+            'dataset-actions',
+            'r2-messages',
+            'file-list',
+            'mock-config'
+        ]
+        const props = {
+            class: `cage boxes ${this.modalClass} ${this.showCage ? '' : 'hidden'}`
+        }
+        return <div {...props}>{usedBoxesInOrder.map(id => makeBox(id))}</div>
     }
 }
 </script>
+
+<templateDEAKT>
+    <!-- This template now is replaced by the render function above -->
+    <div class="cage boxes" :class="[{ hidden: !showCage }, modalClass]">
+        <box class="animate" :config="boxes['main-nav']" :isModalOverlay="isModalOverlay"></box>
+        <box class="animate" :config="boxes['head-controls']" :isModalOverlay="isModalOverlay"></box>
+        <!--  -->
+        <box class="animate" :config="boxes['landing-page']" :isModalOverlay="isModalOverlay"></box>
+        <box class="animate" :config="boxes['search-page']" :isModalOverlay="isModalOverlay"></box>
+        <box class="animate" :config="boxes['search-page-facets-dn-inner']" :isModalOverlay="isModalOverlay"></box>
+        <box class="animate" :config="boxes['mywork-page']" :isModalOverlay="isModalOverlay"></box>
+        <!--  -->
+        <box class="animate" :config="boxes['dataset-view-public']" :isModalOverlay="isModalOverlay"></box>
+        <box class="animate" :config="boxes['dataset-view-private-content']" :isModalOverlay="isModalOverlay"></box>
+        <box class="animate" :config="boxes['dataset-view-private-infos']" :isModalOverlay="isModalOverlay"></box>
+        <!--  -->
+        <box class="animate" :config="boxes['dataset-controls']" :isModalOverlay="isModalOverlay"></box>
+        <box class="animate" :config="boxes['dataset-actions']" :isModalOverlay="isModalOverlay"></box>
+        <box class="animate" :config="boxes['r2-messages']" :isModalOverlay="isModalOverlay"></box>
+        <!--  -->
+        <box class="animate" :config="boxes['file-list']" :isModalOverlay="isModalOverlay"></box>
+
+        <box class="animate" :config="boxes['mock-config']" :isModalOverlay="isModalOverlay"></box>
+    </div>
+</templateDEAKT>
 
 <style lang="scss" scoped>
 .hidden {
